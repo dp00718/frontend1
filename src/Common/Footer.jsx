21392f6621b82bf26502d2a80606960883e4b136
dp00718 <156597537+dp00718@users.noreply.github.com>
2024-08-25 22:15:12 +0530

    index

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -51,7 +51,7 @@ const {handelclick3,handelclick4,handelclick5,handelclick6,handelclick8} = useCo
     <div className='footer_main3'>
     <div className='footer_main3_first' onClick={() => window.location.href='/'}>NIR .</div>
     <div className='footer_main3_second'>
-    <div className='second_main1' onClick={handelclick8}>Contact-Us</div>
+    <div className='third_main1' onClick={handelclick8}>Contact-Us</div>
     </div>
     </div>
 
